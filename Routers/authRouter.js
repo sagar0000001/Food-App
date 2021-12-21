d2a6f55c79b1c3d9380c046b914e37e6b3271228
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2021-12-21 08:59:28 +0530

    Router for authentication section. MongoDb treats ../ as malicious path, Keep attention while sending files as response

---
+++ Routers/authRouter.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let path = require('path');
+let authRouter = express.Router();
+let userModel = require('../models/userModel');
+
+authRouter
+    .route('/signup')
+    .get(getSignUp)
+    .post(postSignUp)
+
+async function postSignUp(req, res) {
+    let data = req.body;
+    let user = await userModel.create(data);
+
+    res.json({
+        message: "you are signed up",
+        YourDataInOurDb: user
+    });
+}
+
+function getSignUp(req, res) {
+    res.sendFile('signup.html', { root: './public' }) // For mongoDB (😁like that only)
+    // res.sendFile('../public/signup.html', { root: __dirname })
+}
+
+module.exports = authRouter;
