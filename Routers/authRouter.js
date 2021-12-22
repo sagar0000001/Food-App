9c3c15e9c0e5e907ef84717fc1aac354fefd6096
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2021-12-22 22:33:36 +0530

    auth login -for unhashed stored password in db , and used safety measures try catch block and handled null fields from frontend

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,12 +1,50 @@
 let express = require('express');
-let path = require('path');
 let authRouter = express.Router();
 let userModel = require('../models/userModel');
+let bcrypt = require('bcrypt')
 
 authRouter
     .route('/signup')
     .get(getSignUp)
     .post(postSignUp)
+
+authRouter
+    .route('/login')
+    .post(loginUser)
+
+
+async function loginUser(req, res) {
+    try { // validating data from frontend
+        let data = req.body;
+        if (data.email) {
+            let user = await userModel.findOne({ email: data.email });
+            console.log(user);
+
+            if (user) {
+                // let salt = await bcrypt.genSalt(); // giving promise
+                // let hashedString = await bcrypt.hash(data.password, salt);
+                if (user.password == data.password) {
+                    res.send("you are logged in")
+                }
+                else res.send("password incorrect")
+            }
+            else { // user := null
+                res.send("user not found");
+            }
+        }
+        else {
+            res.json({
+                message: "empty fields found" //email not entered
+            })
+        }
+
+    } catch (err) {
+        res.json({
+            err: err.message
+        })
+    }
+
+}
 
 async function postSignUp(req, res) {
     let data = req.body;
@@ -22,5 +60,6 @@ function getSignUp(req, res) {
     res.sendFile('signup.html', { root: './public' }) // For mongoDB (🤔like that only)
     // res.sendFile('../public/signup.html', { root: __dirname })
 }
+
 
 module.exports = authRouter;
