5de888884bad325ec21680c9e387a527fe744f7c
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2021-12-23 14:11:41 +0530

    Validate user by Cookies using JWT

--- Routers/authHelper.js
+++ Routers/authHelper.js
@@ -1,12 +1,23 @@
+const jwt = require('jsonwebtoken');
+let JWT_KEY = "sdjr8ere09u" //random (our choice)
+
 //🔑logged in or not
 // if logged out->🛑not granted to enter
 // if logged in->🙏u are welcome
 //So we'll use Cookies🍪
 function protectRoute(req, res, next) {
     if (req.cookies.isLoggedIn) {
-        next();
+        let isVerified = jwt.verify(req.cookies.isLoggedIn, JWT_KEY);
+        if (isVerified) {
+            next();
+        }
+        else {
+            res.json({
+                message: "user has no valid keys"
+            })
+        }
     }
-    else {
+    else { //there is no cookie stored named as 'isLoggedIn'
         res.json({
             message: "operation not allowed"
         })
--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -2,6 +2,8 @@ let express = require('express');
 let authRouter = express.Router();
 let userModel = require('../models/userModel');
 let bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+let JWT_KEY = "sdjr8ere09u" //random (our choice)
 
 authRouter
     .route('/signup')
@@ -13,19 +15,34 @@ authRouter
     .post(loginUser)
 
 
-async function loginUser(req, res) {
+async function loginUser(req, res) { // from email, password
     try { // validating data from frontend
         let data = req.body;
         if (data.email) {
-            let user = await userModel.findOne({ email: data.email });
+            let user = await userModel.findOne({ email: data.email }); // from database🏪
             console.log(user);
 
             if (user) {
                 // let salt = await bcrypt.genSalt(); // giving promise
                 // let hashedString = await bcrypt.hash(data.password, salt);
                 if (user.password == data.password) {
-                    res.cookie("isLoggedIn", true)
+                    let uid = user['_id']; //unique id🆔 ,inside mongoDB🏪
+                    let jwt_token = jwt.sign({ payload: uid }, JWT_KEY); //creating signature🔑
+                    // res.cookie("isLoggedIn", true)
+                    res.cookie("isLoggedIn", jwt_token)
                     res.send("you are logged in")
+
+                    /* Our jwt token is loking like this
+                    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+                    eyJwYXlsb2FkIjoiNjFiZDJiOGY1YTJiOWUwZDQ3OGZhZWVlIiwiaWF0IjoxNjQwMjQ4MTI0fQ.
+                    Gjz4voOS9-V5AiYQgDosGqCV-mm15TeTLGgy5xG32X8 
+                    
+                    This is our Signature made up of
+                    1) Header (encryption algorithm) ->here the default encryptn = H256 
+                    2) Payload (unique id) ->from our database
+                    3) Secret key ->which is random -> a string of our choice
+                    
+                    */
                 }
                 else res.send("password incorrect")
             }
